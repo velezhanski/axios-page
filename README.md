ff68d5502fd247602242daf59de4502376389944
Alex Filipenko <alex@MacBook-Pro-Alex.local>
2020-04-29 11:41:43 +0300

    Updated readme and gulp settings

--- README.md
+++ README.md
@@ -3,15 +3,12 @@
 ## About The Project
 This was built for an interview test, it's a simple website compiled with gulp. Let's hope that it will land me the job :smile:
 
-You may view the live Demo here (No PHP support) -> 
-<a href="https://axios-page.netlify.app/" target="_blank"><img src="https://temudokter.netlify.com/static/media/netlify.92b6c228.png" alt="Live Demo" width="50px" height="50px" valign="middle"></a>
-
 ## Getting Started
 First of all you will need to install the node_modules
 ```
 npm install
 ```
-And then run the following gulp command to launch in browser
+And then run the following gulp command to launch the web server in the browser
 ```
 gulp start
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -15,28 +15,43 @@ gulp.task('sass', function () {
     }));
 });
 
-gulp.task('build', gulp.series(('sass'), function() {
-  return gulp.src(['app/*.html', 'app/images/**/*.png', 'app/js/**/*.js', 'app/php/**/*.php'], { base: './app' })
+gulp.task('build', gulp.series(('sass'), function () {
+  return gulp.src(['app/*.html', 'app/images/**/*.png', 'app/js/**/*.js', 'app/php/**/*.php'], {
+      base: './app'
+    })
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.reload({
       stream: true
     }));
 }));
 
-gulp.task('php', function(){
-  php.server({base:'./dist', port:8010, keepalive:true});
+gulp.task('php', function () {
+  php.server({
+    base: './dist',
+    port: 8010,
+    keepalive: true
+  });
 });
 
-gulp.task('start', gulp.series((['build', 'php']), function (done) {
+gulp.task('debug', gulp.series(('build'), function (done) {
   browserSync.init({
-    proxy:"localhost:8010",
-    baseDir: "./dist",
-    open:true,
-    notify:false
-});
+    server: {
+      baseDir: 'dist'
+    },
+  });
 
   gulp.watch("app/scss/**/*.scss", gulp.series('sass'));
   gulp.watch("app/*.html", gulp.series('build'));
   gulp.watch("app/js/**/*.js", gulp.series('build'));
+  done();
+}));
+
+gulp.task('start', gulp.series((['build', 'php']), function (done) {
+  browserSync.init({
+    proxy: "localhost:8010",
+    baseDir: "./dist",
+    open: true,
+    notify: false
+  });
   done();
 }));
